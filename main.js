1782d2ad212734b89a4f35f6aad96b986a66a933
Wendel Duarte <wendel.duarte@proton.me>
2025-01-14 00:51:34 -0300

    added renderer.js and preload.js

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-const { app, BrowserWindow } = require("electron/main");
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("client/index.html");
 };
 
 app.whenReady().then(() => {
+  ipcMain.handle("ping", () => "pong");
+
   createWindow();
 
   app.on("activate", () => {
