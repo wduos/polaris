dcd1889d1c4bf3a2664119abc0d03f11981b980a
Wendel Duarte <wendel.duarte@proton.me>
2025-01-14 01:50:01 -0300

    added Ubuntu font and initial splash-screen

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
--- preload.js
+++ preload.js
@@ -6,3 +6,7 @@ contextBridge.exposeInMainWorld("versions", {
   electron: () => process.versions.electron,
   ping: () => ipcRenderer.invoke("ping"),
 });
+
+contextBridge.exposeInMainWorld("API", {
+  // insert logic here
+});
